ceb358300d33ee694d759fc432743b10fe7e33b0
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-09 13:58:37 +0100

    0 tot 10

--- helloworld.php
+++ helloworld.php
@@ -1,26 +1,13 @@
 <?php
-$getal1=15;
-$getal2=3;
+$teller=1;
+$maximum=100;
 
-$som=$getal1 + $getal2;
-echo "$getal1 + $getal2 = ".$som;
-echo "\n"; 
+echo "teller is voor de while lus ".$teller."\n";
 
-$verschil=$getal1 - $getal2;
-echo "$getal1 - $getal2 = " .$verschil;
-echo "\n";
+while ( $teller <= $maximum ) {
+ echo "teller is nu " .$teller."\n";
+$teller++;
+}
 
-$product=$getal1 * $getal2;
-echo "$getal1 * $getal2 = ".$product;
-echo "\n";
-
-$quotient=$getal1 / $getal2;
-echo "$getal1 / $getal2 = ".$quotient;
-echo "\n";
-
-$rest=$getal1 % $getal2;
-
-echo "$getal1 % $getal2 = ".$rest;
-echo "\n";
-
+echo "teller is na de while lus ".$teller."\n";
 ?>
