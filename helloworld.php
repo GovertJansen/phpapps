0c96a12dcb3d725ec03923842035f1e186406ad8
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-09 11:38:37 +0100

    rekenen

--- helloworld.php
+++ helloworld.php
@@ -1,10 +1,26 @@
 <?php
+$getal1=15;
+$getal2=3;
 
-$tekst="hello, world! hallo, wereld!";
+$som=$getal1 + $getal2;
+echo "$getal1 + $getal2 = ".$som;
+echo "\n"; 
 
-echo $tekst;
-echo $tekst;
-echo $tekst;
-echo $tekst;
-echo "Dit is een test";
+$verschil=$getal1 - $getal2;
+echo "$getal1 - $getal2 = " .$verschil;
+echo "\n";
+
+$product=$getal1 * $getal2;
+echo "$getal1 * $getal2 = ".$product;
+echo "\n";
+
+$quotient=$getal1 / $getal2;
+echo "$getal1 / $getal2 = ".$quotient;
+echo "\n";
+
+$rest=$getal1 % $getal2;
+
+echo "$getal1 % $getal2 = ".$rest;
+echo "\n";
+
 ?>
