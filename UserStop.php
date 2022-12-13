89915272720f7eab749a8f314b381b4fd97d01e1
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-13 10:51:43 +0100

    ???

--- UserStop.php
+++ UserStop.php
@@ -1,14 +1,16 @@
 <?php
 
-$tafel=readline("Welke tafel wil je zien?");
-$begin=1;
-$eind=10;
-
-echo "Tafel van $tafel:\n";
-
-for ($teller=$begin;$teller<=$eind;$teller++) {
-    $product=$teller*$tafel;
-    echo $teller." x ".$tafel." = ".$product."\n";
+function func() {
+    echo "+ Deze code is in func() uitgevoerd.\n";
 }
+echo "-Deze code is voor de for-lus uitgevoerd. \n";
 
+for ($counter=1; $counter<=3;$counter++) {
+    echo "= Deze code is in de for-lus\n";
+    echo " maar voor func() uitgevoerd.\n";
+    func();
+    echo "= Deze code is in de for-lus\n";
+    echo " maar na func() uitgevoerd.\n";
+    echo "- Deze code is na de for-lus uitgevoerd.\n";
+}
 ?>
