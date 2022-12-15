0cdb53ebbaf31c1011d24a7aadd7def983b33868
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-15 10:02:40 +0100

    quiz 3

--- Quiz.php
+++ Quiz.php
@@ -1,4 +1,20 @@
 <?php
-$random = rand(1,100)
+$teller = "0";
+$stop = "stop";
+$random = rand(1,100);
+$invoer = readline ("Geef een getal");
+
+while ($random != $invoer) {
+    echo "Dit antwoord is niet juist, probeer het opnieuw.\n";
+if ($random == $invoer) 
+echo "Dit is juist!";
+if ($random < $invoer)
+echo "Het getal is lager";
+if ($random > $invoer)
+echo "Het getal is hoger";
+
+ exit;
+$teller++;
+}
 
 ?>
