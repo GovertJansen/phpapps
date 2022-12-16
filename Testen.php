e2db29ea945bfb13c2e99fc266e37b8278b55013
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-16 13:51:08 +0100

    quiz klaar 16/12/2022

--- Testen.php
+++ Testen.php
@@ -1,27 +1,22 @@
-<?php
-$random = rand(1,100);
-$invoer = readline ("Raad het getal 1 t/m 100.");
-$teller = 0;
+<?php 
 
-while ($random != $invoer) {
-    echo "Niet juist, probeer nog eens\n";
-    if ($invoer < 1 or $invoer > 100) {
-        exit ("Verkeerde invoer");
-    }
-    if ($invoer == "stop") {
-        exit ("Gestopt door gebruiker.");
-    }
-    if ($random < $invoer){ 
+$teller = 0;
+$random = rand(1,100);
+$invoer = 101;
+echo $random;
+while ($invoer != $random){
+    $invoer = readline("Geef een getal");
+    if($invoer=='stop'){
+        exit ("Gestopt");
+    }else if ($invoer < 1 or $invoer > 100){
+        exit ("Verkeerd getal ingevoerd\n");
+    }else if ($invoer < $random){
+        echo "Het getal is hoger\n";
+    }else if ($invoer > $random){
         echo "Het getal is lager\n";
     }
-    if ($random > $invoer){ 
-        echo "Het getal is hoger\n";
-    }
-    $invoer = readline ("Raad het getal");
-    $teller ++;
+    $teller++;
 }
- 
-echo ("Dit is juist. Je hebt het in $teller keer geraden.");
 
-
+echo ("Je hebt het geraden in $teller keer");
 ?>
