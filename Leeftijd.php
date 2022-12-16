e70543be95e2b41a1bb32bb04dffecffe32ae25a
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-16 10:17:02 +0100

    leeftijd

---
+++ Leeftijd.php
@@ -0,0 +1,16 @@
+<?php
+$invoer = readline ("Vul je leeftijd in:");
+
+while ($invoer < 9) {
+exit ("Je bent te jong"); 
+}
+if ($invoer > 118) {
+    exit ("Je bent te oud");
+}
+if ($invoer == 10  || $invoer == 16 || $invoer == 18 || $invoer == 50 || $invoer == 67 || $invoer == 100) {
+    echo ("Gefeliciteerd!");
+} 
+if ($invoer ==  62) {
+    echo ("Waarschuwing!");
+}
+?>
--- Testen.php
+++ Testen.php
@@ -3,13 +3,22 @@ $random = rand(1,100);
 $invoer = readline ("Raad het getal 1 t/m 100.");
 $teller = 0;
 
-while ($random != $invoer) { echo "Niet juist, probeer nog eens\n";
-    if ($invoer < 1 or $invoer > 100) {exit ("Verkeerde invoer");}
-     if ($invoer == "stop") {exit ("Gestopt door gebruiker.");}
-      if ($random < $invoer) echo "Het getal is lager\n";
-       if ($random > $invoer) echo "Het getal is hoger\n";
-        $invoer = readline ("Raad het getal");
-         $teller ++;
+while ($random != $invoer) {
+    echo "Niet juist, probeer nog eens\n";
+    if ($invoer < 1 or $invoer > 100) {
+        exit ("Verkeerde invoer");
+    }
+    if ($invoer == "stop") {
+        exit ("Gestopt door gebruiker.");
+    }
+    if ($random < $invoer){ 
+        echo "Het getal is lager\n";
+    }
+    if ($random > $invoer){ 
+        echo "Het getal is hoger\n";
+    }
+    $invoer = readline ("Raad het getal");
+    $teller ++;
 }
  
 echo ("Dit is juist. Je hebt het in $teller keer geraden.");
