868e93e9ca6fa7909f0a7c8719d2da959857f452
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-19 11:56:11 +0100

    Array

---
+++ Array.php
@@ -0,0 +1,8 @@
+<?php
+$programmeertalen = array ("PHP", "C#", "HTML", "JavaScript", "Java", "C++", "C");
+array_push($programmeertalen, "Python", "Swift");
+sort ($programmeertalen);
+print_r ($programmeertalen);
+
+?>
+
