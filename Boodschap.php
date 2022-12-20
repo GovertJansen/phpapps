44feaa99d1bae22693a0c029e271d27d172534da
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2022-12-20 10:51:54 +0100

    Boodschap 1

---
+++ Boodschap.php
@@ -0,0 +1,11 @@
+<?php
+
+$invoer = readline ("Hoeveel producten wil je plaatsen:\n");
+
+if ($invoer != is_numeric($invoer) ) {
+    echo "Geen getal ingevoerd";
+}
+
+$invoer = readline ("Geef je producten op:");
+
+?>
