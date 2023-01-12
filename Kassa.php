7dd044265b3c8167d013fb80144f2dc6f3109fd3
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2023-01-12 13:17:07 +0100

    Kassa 3

--- Kassa.php
+++ Kassa.php
@@ -15,6 +15,18 @@ echo "Je moet $aantal € betalen.\n";
 
 $betaalmethode = readline("Met welk bedrag betaald u?");
 
-while ($betaalmethode < $aantal) {
-    $betaalmethode = readline("Met welk bedrag betaald u?");
+while ($aantal > 0) {
+    if (!is_numeric($betaalmethode)) {
+        echo ("Dit is geen geldig getal\n");
+        $betaalmethode = readline("Je moet nog $aantal betalen.\n Met welk bedrag betaal je?");
+    } else {
+        $aantal = $aantal - $betaalmethode;
+        if ($aantal > 0) $betaalmethode = readline("Je moet nog $aantal betalen.\n Met welk bedrag betaal je?");
+    }
 }
+
+if ($betaalmethode > $aantal && ($aantal < 0)) {
+    $positive = abs($aantal);
+    echo "Je krijgt $positive terug.\n";
+}
+echo "U heeft betaald.";
