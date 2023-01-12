b3b6def64d11d9a293abf0d0fa31f843bcf9cde7
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2023-01-12 16:45:27 +0100

    Afstand 1

---
+++ X-Afstand.php
@@ -0,0 +1,14 @@
+<?php
+$invoerwaarde = readline("Welke waarde:");
+$invoergrootheid = readline("Welke grootheid:");
+$invoergewenst = readline("Welke gewenste grootheid:");
+
+$eenheden = array(
+    "Kilometer",
+    "Hectometer",
+    "Decameter",
+    "Meter",
+    "Decimeter",
+    "Centimeter",
+    "Milimeter"
+);
