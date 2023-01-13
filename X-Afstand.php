9d00d969f602b6d81e373ac864421779d37f1ff4
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2023-01-13 14:04:20 +0100

    Afstand 2

--- X-Afstand.php
+++ X-Afstand.php
@@ -1,5 +1,5 @@
 <?php
-$invoerwaarde = readline("Welke waarde:");
+$waarde = readline("Welke waarde:");
 $invoergrootheid = readline("Welke grootheid:");
 $invoergewenst = readline("Welke gewenste grootheid:");
 
@@ -12,3 +12,18 @@ $eenheden = array(
     "Centimeter",
     "Milimeter"
 );
+
+if ($invoergrootheid == $eenheden[0])
+    if ($invoergewenst == $eenheden[1]) {
+        echo ($waarde * 10);
+    } elseif ($invoergewenst == $eenheden[2]) {
+        echo ($waarde * 100);
+    } elseif ($invoergewenst == $eenheden[3]) {
+        echo ($waarde * 1000);
+    } elseif ($invoergewenst == $eenheden[4]) {
+        echo ($waarde * 10000);
+    } elseif ($invoergewenst == $eenheden[5]) {
+        echo ($waarde * 100000);
+    } elseif ($invoergewenst == $eenheden[6]) {
+        echo ($waarde * 1000000);
+    }
