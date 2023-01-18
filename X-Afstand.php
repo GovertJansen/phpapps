4ac097809cdbd220183ef1f57adaff9bc5e2896d
GovertJansen <117443756+GovertJansen@users.noreply.github.com>
2023-01-18 15:20:52 +0100

    Afstanden Berekeningen

--- X-Afstand.php
+++ X-Afstand.php
@@ -43,3 +43,79 @@ if ($invoergrootheid == $eenheden[1])
     } elseif ($invoergewenst == $eenheden[6]) {
         echo ($waarde * 100000);
     }
+
+
+if ($invoergrootheid == $eenheden[2])
+    if ($invoergewenst == $eenheden[0]) {
+        echo ($waarde / 100);
+    } elseif ($invoergewenst == $eenheden[1]) {
+        echo ($waarde / 10);
+    } elseif ($invoergewenst == $eenheden[3]) {
+        echo ($waarde * 10);
+    } elseif ($invoergewenst == $eenheden[4]) {
+        echo ($waarde * 100);
+    } elseif ($invoergewenst == $eenheden[5]) {
+        echo ($waarde * 1000);
+    } elseif ($invoergewenst == $eenheden[6]) {
+        echo ($waarde * 10000);
+    }
+
+if ($invoergrootheid == $eenheden[3])
+    if ($invoergewenst == $eenheden[0]) {
+        echo ($waarde / 1000);
+    } elseif ($invoergewenst == $eenheden[1]) {
+        echo ($waarde / 100);
+    } elseif ($invoergewenst == $eenheden[2]) {
+        echo ($waarde / 10);
+    } elseif ($invoergewenst == $eenheden[4]) {
+        echo ($waarde * 10);
+    } elseif ($invoergewenst == $eenheden[5]) {
+        echo ($waarde * 100);
+    } elseif ($invoergewenst == $eenheden[6]) {
+        echo ($waarde * 1000);
+    }
+
+if ($invoergrootheid == $eenheden[4])
+    if ($invoergewenst == $eenheden[0]) {
+        echo ($waarde / 10000);
+    } elseif ($invoergewenst == $eenheden[1]) {
+        echo ($waarde / 1000);
+    } elseif ($invoergewenst == $eenheden[2]) {
+        echo ($waarde / 100);
+    } elseif ($invoergewenst == $eenheden[3]) {
+        echo ($waarde / 10);
+    } elseif ($invoergewenst == $eenheden[5]) {
+        echo ($waarde * 10);
+    } elseif ($invoergewenst == $eenheden[6]) {
+        echo ($waarde * 100);
+    }
+
+if ($invoergrootheid == $eenheden[5])
+    if ($invoergewenst == $eenheden[0]) {
+        echo ($waarde / 100000);
+    } elseif ($invoergewenst == $eenheden[1]) {
+        echo ($waarde / 10000);
+    } elseif ($invoergewenst == $eenheden[2]) {
+        echo ($waarde / 1000);
+    } elseif ($invoergewenst == $eenheden[3]) {
+        echo ($waarde / 100);
+    } elseif ($invoergewenst == $eenheden[4]) {
+        echo ($waarde / 10);
+    } elseif ($invoergewenst == $eenheden[6]) {
+        echo ($waarde * 10);
+    }
+
+if ($invoergrootheid == $eenheden[6])
+    if ($invoergewenst == $eenheden[0]) {
+        echo ($waarde / 1000000);
+    } elseif ($invoergewenst == $eenheden[1]) {
+        echo ($waarde / 100000);
+    } elseif ($invoergewenst == $eenheden[2]) {
+        echo ($waarde / 10000);
+    } elseif ($invoergewenst == $eenheden[3]) {
+        echo ($waarde / 1000);
+    } elseif ($invoergewenst == $eenheden[4]) {
+        echo ($waarde / 100);
+    } elseif ($invoergewenst == $eenheden[5]) {
+        echo ($waarde / 10);
+    }
