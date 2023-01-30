842b90ef9674ac6a22a6a239bcf6ff77eba02e32
Govert <97050782@st.deltion.nl>
2023-01-30 12:53:07 +0100

    live klok

--- M-Klok.php
+++ M-Klok.php
@@ -1,6 +1,7 @@
 <?php
 set_time_limit(60);
 for ($i = 0; $i < 59; ++$i) {
-    echo date("d-m-Y h:i:s\n");
-    sleep(10);
+    echo date("d-m-Y h:i:s");
+    echo "\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08\x08";
+    sleep(1);
 }
