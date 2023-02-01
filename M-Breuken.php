70c6f064bb90e9bedeb2a53f7ee4df13d9202c89
Govert <97050782@st.deltion.nl>
2023-02-01 16:57:21 +0100

    breuken 2

--- M-Breuken.php
+++ M-Breuken.php
@@ -1,5 +1,20 @@
 <?php
+
 $Deeltal = readline("Geef een deeltal:");
 $Deler = readline("Geef een Deler:");
 $Antwoord = ($Deeltal / $Deler);
-echo "$Antwoord";
+
+
+if (is_int($Antwoord)) {
+    echo "Kaas";
+}
+
+die;
+// Check if the type of a variable is integer   
+$x = 5985;
+var_dump(is_int($x));
+
+
+// Check again... 
+$x = 59.85;
+var_dump(is_int($x));
